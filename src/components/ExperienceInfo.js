17eca3b96de270a4088375267908d9aa18196aa9
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-11-08 12:43:28 -0300

    responsividad

--- src/components/ExperienceInfo.js
+++ src/components/ExperienceInfo.js
@@ -3,7 +3,7 @@ export const experienceInfo = [
     role: "Freelance - Desarrollador web Front end",
     duration: "2024 - Presente",
     responsibilities:
-      "Responsable de diseñar, desarrollar y mejorar aplicaciones web que funcionen bien y que ofrezcan experiencias únicas. Con un enfoque en cubrir necesidades específicas de los usuarios, centrándome en crear aplicaciones que combinan funcionalidad y una calidad de experiencia destacada.",
+      "Responsable de diseñar, desarrollar y mejorar aplicaciones web que funcionen bien y que ofrezcan experiencias únicas.\n \n Con un enfoque en cubrir necesidades específicas de los usuarios, centrándome en crear aplicaciones que combinan funcionalidad y una calidad de experiencia destacada.",
     skillsUsed: ["Javascript", "AstroJs", "TailwindCSS"],
   },
   {
