27d933911aa48813b39cde1ac5eb63b44775d87d
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-11-07 23:02:18 -0300

    projects

---
+++ src/components/ProjectsInfo.js
@@ -0,0 +1,36 @@
+export const projectsInfo = [
+  {
+    project: "Página Web - Colegio Alcázares de Ñuble",
+    img: "../../public/colegio-alcazares.png",
+    description:
+      "Aplicación web en la cual es utilizada para mantener informada a la comunidad educativa del colegio",
+    skillsUsed: ["Javascript", "AstroJs", "TailwindCSS"],
+    alt: "Colegio Alcázares de Ñuble",
+    href: "https://colegioalcazaresdenuble.cl/",
+  },
+  {
+    project: "Página Web - Comunidad Clan (En desarrollo)",
+    img: "../../public/comunidad-clan.png",
+    description:
+      "Aplicación web en la cual es utilizada para mantener informada a la comunidad educativa del colegio",
+    skillsUsed: ["Javascript", "AstroJs", "TailwindCSS"],
+    alt: "Comunidad Clan",
+    href: "https://comunidadclan.cl/",
+  },
+  {
+    project: "Página Web - Tornegors",
+    img: "../../public/comunidad-clan.png",
+    description:
+      "Aplicación web en la cual es utilizada para mantener informada a la comunidad educativa del colegio",
+    skillsUsed: [
+      "Javascript",
+      "React",
+      "TailwindCSS",
+      "NodeJS",
+      "PostgreSQL",
+      "Express",
+    ],
+    alt: "Comunidad Clan",
+    href: "https://comunidadclan.cl/",
+  },
+];
