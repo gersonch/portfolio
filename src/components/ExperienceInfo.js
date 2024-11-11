60e20cb8d24feb2f41248d9f7ec37280aaffff1e
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-11-11 16:09:30 -0300

    arreglar detalles

--- src/components/ExperienceInfo.js
+++ src/components/ExperienceInfo.js
@@ -10,7 +10,7 @@ export const experienceInfo = [
     role: "Colegio Alcázares de Ñuble - Encargado sala de enlaces",
     duration: "2024 - Presente",
     responsibilities:
-      "Encargado del desarrollo y mantenimiento de la página oficial del colegio, con el objetivo de mantener a la comunidad educativa informada sobre todos los eventos y actividades de la institución.\n \n Además, responsable de la manutención de los artículos electrónicos del establecimiento, tales como computadores, impresoras, routers, entre otros dispositivos, asegurando su correcto funcionamiento para el soporte de las actividades diarias del colegio.",
+      "Encargado del desarrollo y mantenimiento de la página oficial del colegio, con el objetivo de mantener a la comunidad educativa informada sobre todos los eventos y actividades de la institución.\n \n Además, responsable del mantenimiento de los artículos electrónicos del establecimiento, tales como computadores, impresoras, routers, entre otros dispositivos, asegurando su correcto funcionamiento para el soporte de las actividades diarias del colegio.",
     skillsUsed: ["Javascript", "AstroJs", "TailwindCSS", "Wordpress"],
   },
 ];
